c72c67dbac496bd9110cdae993108878e7d7f025
UmeshNebel <dashandeshmukh15@gmail.com>
2026-06-15 12:40:53 +0530

    Add Dto class for get, post, put and Mapped with model and also register service inside program.cs

---
+++ StudentDemo/DTO/StudentDTO.cs
@@ -0,0 +1,14 @@
+﻿namespace StudentDemo.DTO
+{
+    // DTO class for getStudent data
+    public class StudentDTO
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public int Age { get; set; }
+
+        public string Email { get; set; }
+    }
+}
---
+++ StudentDemo/DTO/UpdateStudentDTO.cs
@@ -0,0 +1,15 @@
+﻿namespace StudentDemo.DTO
+{
+    public class UpdateStudentDTO
+    {
+
+        public string Name { get; set; }
+
+        public int Age { get; set; }
+
+        public string Email { get; set; }
+
+        public string Password { get; set; }
+
+    }
+}
